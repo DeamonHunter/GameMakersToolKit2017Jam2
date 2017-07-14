054a70aa2200f4a07a6b75cde13d5d0de1db8223
Deamonhunter <deamonhunterextra@outlook.com>
2017-07-14 23:58:32 +1000

    Reorganise files. Added lance sprite. Added A BaseWeaponScript.

---
+++ Assets/Scripts/Weapons/BaseWeaponScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseWeaponScript : MonoBehaviour {
+    public Vector2 StartOffset;
+    public PlayerController Player;
+    public float AttackForce;
+    public float AttackTimeout;
+
+    public bool UseHeat; //When using guns
+
+    public float staminaUse;
+    public float StaminaGainFromEnemy;
+
+    protected bool _attacking;
+    protected float _lastAttack;
+
+
+    // Use this for initialization
+    void Start() {
+        Player = transform.parent.gameObject.GetComponent<PlayerController>();
+        transform.localPosition = StartOffset;
+    }
+
+    void Update() {
+        if (_attacking && _lastAttack + AttackTimeout < Time.time) {
+            _attacking = false;
+        }
+    }
+
+    public abstract float Attack();
+}
