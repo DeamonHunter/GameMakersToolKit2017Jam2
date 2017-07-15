37f0c473db985682dc181c3c8ffe6c4746e11af1
Deamonhunter <deamonhunterextra@outlook.com>
2017-07-15 10:44:10 +1000

    Added very basic level design system. Added doors with particle effects.

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+    public bool DoorOpen;
+    public float TimetoOpenDoor;
+
+    public Color DoorOpenColor;
+    public Color DoorClosedColor;
+    public ParticleSystem[] doorParticles;
+
+    private BoxCollider2D box;
+    private SpriteRenderer sprite;
+    private float progress;
+
+    void Start() {
+        box = GetComponent<BoxCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        progress = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (DoorOpen) {
+            if (progress < 1) {
+                if (doorParticles[0].isStopped) {
+                    doorParticles[0].Play();
+                    doorParticles[1].Play();
+                }
+                progress += Time.deltaTime / TimetoOpenDoor;
+                sprite.color = Color.Lerp(DoorClosedColor, DoorOpenColor, progress);
+            }
+            else {
+                progress = 1;
+                sprite.color = DoorOpenColor;
+            }
+        }
+        else {
+            if (progress > 0) {
+                if (doorParticles[0].isStopped) {
+                    doorParticles[0].Play();
+                    doorParticles[1].Play();
+                }
+                progress -= Time.deltaTime / TimetoOpenDoor;
+                sprite.color = Color.Lerp(DoorClosedColor, DoorOpenColor, progress);
+            }
+            else {
+                progress = 0;
+                sprite.color = DoorClosedColor;
+            }
+        }
+
+        box.enabled = progress < 0.6;
+    }
+}
---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+    public DoorScript[] Doors;
+    public Transform[] LevelCentres;
+    public GameObject[] LevelDesigns;
+    public GameObject[] Enemies;
+
+    private List<GameObject> SpawnedEnemies;
+    private int CurrentDoor;
+
+    // Use this for initialization
+    void Start() {
+        SpawnedEnemies = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (SpawnedEnemies.Count == 0) {
+            CurrentDoor = Random.Range(0, 3);
+            Doors[CurrentDoor].DoorOpen = true;
+            SpawnedEnemies.Add(Instantiate(Enemies[0], Vector3.zero, Quaternion.identity));
+        }
+    }
+}
