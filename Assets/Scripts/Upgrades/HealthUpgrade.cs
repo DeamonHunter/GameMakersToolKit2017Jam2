685ebb0a962685beec6dda5393481e6cb26cd09a
Ripleigh <ripleighgartside@outlook.com>
2017-07-15 13:26:06 +1000

    health upgrade

--- Assets/Scripts/Upgrades/HealthUpgrade.cs
+++ Assets/Scripts/Upgrades/HealthUpgrade.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class HealthUpgrade : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public PlayerController pc;
+    public GameObject upgradeEffect;
+
+    // Use this for initialization
+    void Start () {
+        pc.GetComponent<PlayerController>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.transform.tag == "Player") {
+            pc.MaxHealth += 1;
+            Instantiate(upgradeEffect, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
 }
