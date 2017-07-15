e2071fdb27aee31e39a217264daab2bff6e7cc98
Deamonhunter <deamonhunterextra@outlook.com>
2017-07-15 21:03:05 +1000

    Shop confirmation text.

---
+++ Assets/Scripts/ShopMessageScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopMessageScript : MonoBehaviour {
+    public float Timeout;
+
+    private float timeoutTime;
+    private Text text;
+
+    // Update is called once per frame
+    void Update() {
+        if (Time.time > timeoutTime)
+            gameObject.SetActive(false);
+    }
+
+    public void Showtext(string message) {
+        if (text == null)
+            text = GetComponent<Text>();
+        text.text = message;
+        timeoutTime = Time.time + Timeout;
+    }
+}
