85eb667441a3c5e16e99a3af10dfb4a5a78a52d7
Ripleigh <ripleighgartside@outlook.com>
2017-07-15 11:42:25 +1000

    adding force (well trying)

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour {
+
+    int force = 100;
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Rigidbody2D>().AddForce(transform.position * force, ForceMode2D.Impulse);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
