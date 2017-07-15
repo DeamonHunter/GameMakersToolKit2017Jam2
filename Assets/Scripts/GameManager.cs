0cb85d198ac4460c64ef820788383c2b172cfa5e
Ripleigh <ripleighgartside@outlook.com>
2017-07-15 15:09:36 +1000

    can press escape to reach main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
@@ -13,6 +14,11 @@ public class GameManager : MonoBehaviour {
 
         else if (instance != this)
             Destroy(gameObject);
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            SceneManager.LoadScene(0);
     }
 
 
