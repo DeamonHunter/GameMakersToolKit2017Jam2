555c2f8f83e68abc90360cea3aff7c5a84fab0fd
Deamonhunter <deamonhunterextra@outlook.com>
2017-07-16 14:29:11 +1000

    Add more stuff. Make level design rotate.

---
+++ Assets/HurtPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtPlayer : MonoBehaviour {
+    public float HurtCooldown;
+    public float Damage;
+    private float lastHurtTime;
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay2D(Collider2D other) {
+        if (other.tag == "Player") {
+            if (lastHurtTime < Time.time) {
+                lastHurtTime = Time.time + HurtCooldown;
+                other.GetComponent<PlayerController>().TakeDamage(Damage);
+            }
+        }
+    }
+}
