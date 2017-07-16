ff8de8a9a95098bbf1786c53f9f1280c55070a11
Deamonhunter <deamonhunterextra@outlook.com>
2017-07-16 15:18:00 +1000

    SAVE THE FRAMES!

--- Assets/Scripts/Enemies/Enemy_3.cs
+++ Assets/Scripts/Enemies/Enemy_3.cs
@@ -5,26 +5,26 @@ using UnityEngine;
 public class Enemy_3 : EnemyBase {
 
     public Vector2 MaxForce;
+    private Vector2 currentSpeed;
 
     float force = 5f;
 
+    private Rigidbody2D rb;
+
     // Update is called once per frame
     protected override void Update() {
-
+        if (Activated)
+            rb.velocity = currentSpeed;
     }
 
     void Start() {
-        if (Activated) {
-            Vector2 rand = new Vector2(Random.Range(-MaxForce.x, MaxForce.x), Random.Range(-MaxForce.y, MaxForce.y));
-            this.GetComponent<Rigidbody2D>().AddForce(rand * force, ForceMode2D.Impulse);
-        }
+        rb = GetComponent<Rigidbody2D>();
+        currentSpeed = new Vector2(Random.Range(-MaxForce.x, MaxForce.x), Random.Range(-MaxForce.y, MaxForce.y));
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        if (Activated) { 
-        Vector2 rand = new Vector2(Random.Range(-MaxForce.x, MaxForce.x), Random.Range(-MaxForce.y, MaxForce.y));
-        this.GetComponent<Rigidbody2D>().AddForce(rand * force, ForceMode2D.Impulse);
-        }
+        if (Activated)
+            currentSpeed = new Vector2(Random.Range(-MaxForce.x, MaxForce.x), Random.Range(-MaxForce.y, MaxForce.y));
     }
 
 }
